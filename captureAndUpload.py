ca8fa022011264cd6d18bee9f346351df1edcb86
HridyanshSangal <84910535+HridyanshSangal@users.noreply.github.com>
2022-06-01 17:40:13 +0530

    Add files via upload

---
+++ captureAndUpload.py
@@ -0,0 +1,37 @@
+import cv2
+import dropbox
+import time
+import random
+start_time = time.time()
+
+def take_snapshot():
+    number = random.randint(0,100)
+    videoCaptureObject = cv2.VideoCapture(0)
+    result = True
+    while(result):
+        ret,frame = videoCaptureObject.read()
+        img_name = 'img'+str(number)+'.png'
+        cv2.imwrite(img_name,frame)
+        start_time = time.time
+        result = False
+    return(img_name)
+    print('Snapshot Taken')
+    videoCaptureObject.release()
+    cv2.destroyAllWindows()
+
+def upload_file(img_name):
+    access_token = 'sl.BItFkwLSEyS4kxjejD37QrGUKk3iqUXsLXHp-S44rKdgo6QIKdQk__zS9OQwwfLpwx6DDkHn5IkDocdyrsc_Ourk_nGk9lhwJSi2XiWzPLXoVYLhu3fStnJwmVcvGSrzt8ttaHS4MHQq'
+    file = img_name
+    file_from = file
+    file_to = '/testFolder/'+(img_name)
+    dbx = dropbox.Dropbox(access_token)
+    with open(file_from,'rb')as f:
+        dbx.files_upload(f.read(),file_to,mode = dropbox.files.WriteMode.overwrite)
+        print('File Uploaded')
+
+def main():
+    while(True):
+        if((time.time()-start_time)>=5):
+            name = take_snapshot()
+            upload_file(name)
+main()
